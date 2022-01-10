6c0d4754eb7e186f44b653e0748683dec2d2b89a
jose-gabriel-bm <josegabrielbm@outlook.com>
2022-01-10 15:22:01 -0300

    Configuração inicial do projeto.

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+
+<index />
+
+</template>
+
+<script>
+import index from './components/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    index,
+  }
+}
+</script>
+
+<style>
+
+</style>
