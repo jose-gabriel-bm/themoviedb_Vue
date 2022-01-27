10c5a77383049694ed125b981d68d32a90216958
jose-gabriel-bm <josegabrielbm@outlook.com>
2022-01-27 11:28:33 -0300

    Correção card de filmes, adicao button de top

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
     <MenuSuperior />
-    <Visualizar />
-    <!-- <Home /> -->
+    <!-- <Visualizar /> -->
+    <Home />
     <Rodape />
   </div>
 </template>
 
 <script>
 import MenuSuperior from './components/MenuSuperior.vue'
-import Visualizar from './views/Visualizar.vue'
-// import Home from './views/Home.vue'
+// import Visualizar from './views/Visualizar.vue'
+import Home from './views/Home.vue'
 import Rodape from './components/Rodape.vue'
 
 
@@ -18,8 +18,8 @@ export default {
   name: 'App',
   components: {
     MenuSuperior,
-    // Home,
-    Visualizar,
+    Home,
+    // Visualizar,
     Rodape,
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,21 +14,12 @@
                 </div>
 
                 <div class="bottomCard">
-
-                    <div id="title">
-                        <h5>
-                            {{result.title}}
-                        </h5>
-                    </div>
-
                     <h5>
                         Data Lançamento
                     </h5>
-
                     <p>
                         {{ result.release_date }}
                     </p>
-
                 </div>
 
             </div> 
@@ -39,6 +30,11 @@
                     id="buttonCarregarMais" type="button">
                     <h4>Carregar Filmes</h4>
                 </button> 
+
+                <button @click.prevent.stop="ReturnTop()" 
+                    id="top">
+                    <h4>Topo</h4>
+                </button>
             </div>          
         </div> 
 
@@ -57,6 +53,7 @@ import axios from 'axios';
             return{
                 results:'',
                 moreMovies:'',
+                page: 2,
             }
         },
         methods:{
@@ -67,15 +64,21 @@ import axios from 'axios';
                 })
             },
             MoreMovies(){
-                axios.get('https://api.themoviedb.org/3/movie/popular?api_key=beed4e65bca0365111bd1076df78d4aa&language=pt-BR&page=2')
+                axios.get('https://api.themoviedb.org/3/movie/popular?api_key=beed4e65bca0365111bd1076df78d4aa&language=pt-BR&page='+ this.page)
                 .then(response => {
                     this.moreMovies = response.data.results
+
+                    for (let i = 0; i < this.moreMovies.length; i++){     
+                        this.results.push(this.moreMovies[i])
+                    }
                 }).finally(() => {
-                    this.moreMovies.map(function(item){
-                        this.results.push({21:item});
-                    })
-                });
+                    this.page = this.page + 1;
+                });    
             },
+            ReturnTop(){
+                window.scrollTo(0, 0);
+            }
+
 
         }
     }
@@ -118,46 +121,54 @@ import axios from 'axios';
         border-radius: 15px 15px 0px 0px;
     }
     .bottomCard {
-        padding: 15px;
+        padding: 10px;
         width: 100%;
         height: 100%;
     }
     .bottomCard h5 {
         margin: 0px;
+        margin-top: 5px;
         cursor: pointer;
         color: rgb(3, 63, 131);
     }
     .bottomCard p {
-        margin: 0px;
+        margin-top: 5px;
+        margin-bottom: 0px;
         color: rgb(20, 54, 94);
-    }
-    #title{
-        margin-bottom: 5px;
-        width: 100%;
-        height: 50px;
     }
     .buttonCarregar{
         text-align: center;
         margin-top: 40px;
     }
-    #buttonCarregarMais h4{
-        margin: 0px;
+    .buttonCarregar button{
+        padding: 15px;
+        text-align: center;
+        border-radius: 10px;
+        border-color: #02a3fa;
+        background-image: linear-gradient(to right, #01507a, #02a3fa);      
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif; 
+        color: #FFF;
     }
-    #buttonCarregarMais:hover{
+    .buttonCarregar button:hover{
         border-radius: 10px;
         border-color: #2a7fad;
         color: rgba(255, 255, 255, 0.863);
         background-image: linear-gradient(to right, #2a7fad, #4692bb);
     }
     #buttonCarregarMais{
-        color: #FFF;
-        float: center;
         width: 600px;
-        padding: 15px;
-        text-align: center;
-        border-radius: 10px;
-        border-color: #02a3fa;
-        background-image: linear-gradient(to right, #01507a, #02a3fa);      
-        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;       
-    }   
+        float: center;
+    } 
+    #top{
+        width: 80px;
+        float: right;
+        margin-right: 20px;
+    }
+    #buttonCarregarMais h4{
+        margin: 0px;
+    }
+    #top h4{
+        margin: 0px;
+    }
+    
 </style>
